b345a467c42eb38820d74972cf84bc7e5be7d25e
Isabel Avila <isabel_vidal@mccormick.com>
2022-04-08 16:19:39 -0500

    Updated dev branch

---
+++ dev/main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project       = var.proj_id
+  region        = var.region
+}
+
+resource "google_storage_bucket" "dev_env" {
+  name          = var.bucket_name
+  project       = var.proj_id
+  #storage_class = var.storage_class
+  location      = var.bucket_region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
---
+++ dev/variables.tf
@@ -0,0 +1,24 @@
+variable "proj_id" {
+  description = "The project ID"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Bucket name"
+  type        = string
+}
+
+variable "region" {
+  description = "The region"
+  type        = string
+}
+
+variable "bucket_region" {
+  description = "The region"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Storage class"
+  type        = string
+}
